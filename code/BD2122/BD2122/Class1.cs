ce5770971997c70413d613fd7649e58afd8048c8
Joao-Felisberto <joaofelisberto@ua.pt>
2022-05-19 16:15:33 +0100

    Added class to communicate with the database

---
+++ code/BD2122/BD2122/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+
+namespace BD2122
+{
+    internal class Class1
+    {
+        private SqlConnection con;
+
+        private SqlConnection getSGBDConnection()
+        {
+            return new SqlConnection("data source= localhost;integrated security=true;initial catalog=Northwind");
+        }
+
+        public void listRecipiesByAuthor(string author)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select * from recipie where recipie.author = @author";
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@author", author);
+            cmd.Connection = con;
+
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
